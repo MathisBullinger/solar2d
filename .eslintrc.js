4a9bf61ba762c1cd1fdb6ce9f7df38bb3b1ddda0
Mathis Bullinger <mathis@bullinger.dev>
2023-03-04 15:16:53 +0100

    setup eslint, prettier, vitest

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+  plugins: ['@typescript-eslint'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    '@typescript-eslint/no-empty-function': 'off',
+  },
+};
